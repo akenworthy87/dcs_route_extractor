66bb0737f5aa053601c0dc7ca34e4fcc45e58dab
Andrew Kenworthy <akenworthy87@gmail.com>
2026-02-11 23:58:32 +0000

    Added error handling for route files with no routes

--- modules/select_routename.py
+++ modules/select_routename.py
@@ -4,6 +4,9 @@
 
 def select_routename(routedata: dict) -> str:
     enumerate_keys = list(enumerate(routedata.keys(), start=1))
+    if len(enumerate_keys) == 0:
+        print("No route names found in route data. Please check your route file and try again.")
+        raise ValueError("No route names found in route data.")
     
     print("Select a route name:")
     for i, key in enumerate_keys:
