8fb89c165440d93c392376bd6dd33703e7252a71
Andrew Kenworthy <akenworthy87@gmail.com>
2026-02-17 15:37:04 +0000

    Improved Terrainspec Dataclass to be frozen

--- modules/process_terrain_spec.py
+++ modules/process_terrain_spec.py
@@ -1,49 +1,49 @@
-from dataclasses import dataclass
-from typing import Optional
+from dataclasses import dataclass, field
 from mgrs import MGRS
 from pyproj import Transformer, CRS
 
 
-@dataclass()
+@dataclass(frozen=True)
 class TerrainSpec:
     origin_mgrs: str
     central_meridian: int
     scale_factor: float
     
     # Calculated fields
-    origin_mgrs_compact: Optional[str] = None
-    lat_origin: Optional[float] = None
-    lon_origin: Optional[float] = None
+    origin_mgrs_compact: str = field(init=False)
+    lat_origin: float = field(init=False)
+    lon_origin: float = field(init=False)
     
-    terrain_proj: Optional[CRS] = None
-    to_terrain_proj: Optional[Transformer] = None
-    from_terrain_proj: Optional[Transformer] = None
+    terrain_proj: CRS = field(init=False)
+    to_terrain_proj: Transformer = field(init=False)
+    from_terrain_proj: Transformer = field(init=False)
 
 
     def make_transformers(self):
         # Conversion method from https://github.com/pydcs/dcs/blob/master/dcs/terrain/projections/transversemercator.py
-        self.terrain_proj = CRS.from_proj4(
-            " ".join(
-                [
-                    "+proj=tmerc",
-                    "+lat_0=0",
-                    f"+lon_0={self.central_meridian}",
-                    f"+k_0={self.scale_factor}",
-                    f"+x_0={self.lon_origin}",
-                    f"+y_0={self.lat_origin}",
-                    "+towgs84=0,0,0,0,0,0,0",
-                    "+units=m",
-                    "+vunits=m",
-                    "+ellps=WGS84",
-                    "+no_defs",
-                    "+axis=neu",
-                ]
+        super().__setattr__(
+            'terrain_proj',
+            CRS.from_proj4(
+                " ".join(
+                    [
+                        "+proj=tmerc",
+                        "+lat_0=0",
+                        f"+lon_0={self.central_meridian}",
+                        f"+k_0={self.scale_factor}",
+                        f"+x_0={self.lon_origin}",
+                        f"+y_0={self.lat_origin}",
+                        "+towgs84=0,0,0,0,0,0,0",
+                        "+units=m",
+                        "+vunits=m",
+                        "+ellps=WGS84",
+                        "+no_defs",
+                        "+axis=neu",
+                    ]
+                )
             ))
 
-        self.to_terrain_proj = Transformer.from_crs(
-            CRS("WGS84"), self.terrain_proj, always_xy=True)   # input is lon,lat
-        self.from_terrain_proj = Transformer.from_crs(self.terrain_proj, CRS(
-            "WGS84"), always_xy=True)  # output is lon,lat
+        super().__setattr__('to_terrain_proj', Transformer.from_crs(CRS("WGS84"), self.terrain_proj, always_xy=True))  # input is lon,lat
+        super().__setattr__('from_terrain_proj', Transformer.from_crs(self.terrain_proj, CRS("WGS84"), always_xy=True))  # output is lon,lat
 
 
     def mgrs_to_latlon(self):
@@ -56,18 +56,20 @@ class TerrainSpec:
             lat_lon = lat_lon.decode()
         # ensure we have floats (some mgrs versions return tuple)
         if isinstance(lat_lon, tuple) and len(lat_lon) == 2:
-            self.lat_origin, self.lon_origin = float(
-                lat_lon[0]), float(lat_lon[1])
+            super().__setattr__('lat_origin', float(lat_lon[0]))
+            super().__setattr__('lon_origin', float(lat_lon[1]))
         else:
             raise RuntimeError("Unexpected return from mgrs.toLatLon")
 
     def __post_init__(self):
         # Validate MGRS format
-        self.origin_mgrs_compact = self.origin_mgrs.replace(" ", "")
+        super().__setattr__('origin_mgrs_compact', self.origin_mgrs.replace(" ", ""))
+        
+        # self.origin_mgrs_compact = self.origin_mgrs.replace(" ", "")
         self.mgrs_to_latlon()
         self.make_transformers()
-        
-        
+
+
 def process_terrain_spec(terrain_spec_data: dict) -> TerrainSpec:
     """
     Takes a terrain spec data dict and processes it into a TerrainSpec object.
