9004dba393a421c7076a948ec6760ce05944a44e
Andrew Kenworthy <akenworthy87@gmail.com>
2026-02-26 01:18:32 +0000

    Added a slight delay to build bat to ensure parts of built program aren't being used by some process, which was breaking the zipping

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 pyinstaller main.spec --clean --noconfirm
 xcopy .\terrain_specs\*.* .\dist\dcs_route_extractor\terrain_specs\ /E /I /Y
+timeout /t 1 /nobreak >nul
 powershell Compress-Archive -Force -Path .\dist\dcs_route_extractor\. -DestinationPath .\dist\dcs_route_extractor.zip
